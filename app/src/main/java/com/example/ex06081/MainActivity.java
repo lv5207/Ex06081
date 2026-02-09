cec5d4496b35e2f91ca22585659f1ad7571055b5
lv5207 <lv5207@bs.amalnet.k12.il>
2026-02-09 09:41:22 +0200

    Commit 2

--- app/src/main/java/com/example/ex06081/MainActivity.java
+++ app/src/main/java/com/example/ex06081/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex06081;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et;
+    private Button btn;
+    private int num;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.et);
+        btn = findViewById(R.id.btn);
     }
 }
